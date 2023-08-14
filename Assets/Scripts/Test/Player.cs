f5562d399add2338b38d0d349e019deac7c1c2ec
MinseokLee <leems041625@gmail.com>
2023-08-14 21:32:56 +0900

    [Feat] : Branch 설명

--- Assets/Scripts/Test/Player.cs
+++ Assets/Scripts/Test/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Yes");
     }
 
     // Update is called once per frame
