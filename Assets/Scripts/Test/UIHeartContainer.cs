855c368c456f4360fdfc4f84df46d6faa87ef903
fixedcow <puren37@gmail.com>
2023-08-16 22:09:02 +0900

    [Feat] : UI 목숨 추가 및 상호작용 추가

---
+++ Assets/Scripts/Test/UIHeartContainer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHeartContainer : MonoBehaviour
+{
+	#region PublicVariables
+	#endregion
+
+	#region PrivateVariables
+	[SerializeField] private List<UIHeart> m_hearts = new List<UIHeart>();
+	private static WaitForSeconds m_heartsGenerateDelay = new WaitForSeconds(0.15f);
+	private int index;
+	#endregion
+
+	#region PublicMethod
+	public void Start()
+	{
+		Initialize();
+	}
+	public void Initialize()
+	{
+		index = 0;
+		foreach (UIHeart heart in m_hearts)
+		{
+			heart.Initialize();
+		}
+	}
+	public void Pop()
+	{
+		m_hearts[index].Pop();
+		++index;
+	}
+	#endregion
+
+	#region PrivateMethod
+	private void HeartCountOut()
+	{
+		Debug.Log("Heart Out!");
+	}
+	#endregion
+}
